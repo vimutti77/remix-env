19f47fa5fcce83231bf9f8145dc5045c44418831
vimutti <vimutti@outlook.com>
2023-05-02 12:39:09 +0700

    breaking changes

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,30 @@
 declare global {
-  var APP_ENV: Record<string, string>;
+  var APP_ENV: Env;
 }
 
-export function createEnv({
-  filterEnv,
-}: { filterEnv?: (key: string, value?: string) => boolean } = {}) {
-  const filteredEnvEntries = Object.entries(process.env).filter(
-    ([key, value]) => {
-      if (filterEnv) return filterEnv(key, value);
-      return key.startsWith("PUBLIC_ENV_");
-    }
-  );
+type Env = Record<string, string | undefined>;
 
-  return {
-    NODE_ENV: process.env.NODE_ENV,
-    ...Object.fromEntries(filteredEnvEntries),
-  };
-}
-
-export function injectEnv(html: string, env: Record<string, string>) {
-  globalThis["APP_ENV"] = env;
-  const stringifyEnv = JSON.stringify(env);
-  const encodedEnv = Buffer.from(stringifyEnv).toString("base64");
-  const envScript = `<script>window['APP_ENV']=JSON.parse(atob("${encodedEnv}"))</script>`;
-  const htmlWithEnv = html.replace("</head>", `${envScript}</head>`);
-  return htmlWithEnv;
+export function setUpEnv() {
+  globalThis["APP_ENV"] = process.env;
 }
 
 export function getEnv() {
   return globalThis["APP_ENV"];
+}
+
+export function injectEnv(
+  html: string,
+  filterPublicEnv?: (key: string, value?: string) => boolean
+) {
+  const env = getEnv();
+  const filteredEnvEntries = Object.entries(env).filter(
+    ([key, value]) =>
+      filterPublicEnv?.(key, value) ?? key.startsWith("PUBLIC_ENV_")
+  );
+
+  const stringifyEnv = JSON.stringify(Object.fromEntries(filteredEnvEntries));
+  const encodedEnv = Buffer.from(stringifyEnv).toString("base64");
+  const envScript = `<script>window['APP_ENV']=JSON.parse(atob("${encodedEnv}"))</script>`;
+  const htmlWithEnv = html.replace("</head>", `${envScript}</head>`);
+  return htmlWithEnv;
 }
